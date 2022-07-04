47327a38b4bce9fc3f92f5b71e794fb59f47ba82
Mathieu <mathieu.collet@gmail.com>
2022-07-04 17:36:40 +0200

    Base style

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: '/interactive-rating-component-main/',
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/assets/style.scss";
+        `
+      }
+    }
+  }
 })
