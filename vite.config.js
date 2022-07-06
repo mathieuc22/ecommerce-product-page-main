9211f630cfd45d1e9d19221ef611fc6281fc6201
Mathieu <mathieu.collet@gmail.com>
2022-07-06 10:31:52 +0200

    Vite config correction for base uri

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/interactive-rating-component-main/',
+  base: '/ecommerce-product-page-main/',
   plugins: [vue()],
   css: {
     preprocessorOptions: {
