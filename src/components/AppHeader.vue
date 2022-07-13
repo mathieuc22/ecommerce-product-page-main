f984a460d5534f3c4417c72e23d96997919809b5
Mathieu <mathieu.collet@gmail.com>
2022-07-13 14:19:05 +0200

    Cart component design

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,16 +16,34 @@
             </ul>
         </nav>
         <nav class="authentication">
-            <ul class="navlist">
-                <li class="navlist__item"><img src="src/assets/images/icon-cart.svg" alt="Icon cart"></li>
-                <li class="navlist__item"><img class="avatar" src="src/assets/images/image-avatar.png"
-                        alt="User avatar"></li>
-            </ul>
+            <div class="navlist">
+                <div class="navlist__cart">
+                    <div @click="cart = !cart" class="navlist__item">
+                        <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+                                fill="#69707D" fill-rule="nonzero" />
+                        </svg>
+                    </div>
+                    <Cart v-if="cart" />
+                </div>
+                <div class="navlist__item">
+                    <img class="avatar" src="src/assets/images/image-avatar.png" alt="User avatar">
+                </div>
+            </div>
         </nav>
     </header>
 </template>
 
-<style lang="scss">
+<script setup>
+import { ref } from 'vue';
+import Cart from './Cart.vue';
+
+const cart = ref(false)
+
+</script>
+
+<style lang="scss" scoped>
 header {
     height: 112px;
     display: flex;
@@ -68,16 +86,25 @@ header {
     justify-content: space-between;
     gap: 2.1rem;
 
+    &__cart {
+        position: relative;
+    }
+
     &__item {
         position: relative;
         height: 100%;
         display: inline-flex;
         align-items: center;
+        cursor: pointer;
 
         &:hover {
-            cursor: pointer;
             color: $color-neutral-Black;
+
+            & svg path {
+                fill: black;
+            }
         }
+
     }
 }
 
@@ -96,7 +123,6 @@ header {
 .hamburger {
     display: none;
 
-
     @media screen and (max-width: 400px) {
         display: flex;
         flex-direction: column;
@@ -111,7 +137,6 @@ header {
 }
 
 .hover-underline {
-    cursor: pointer;
 
     &:after {
         content: "";
---
+++ src/components/Cart.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <div class="cart">
+        <h2 class="cart__title">Cart</h2>
+        <div class="cart__content">
+            <p>Your cart is empty.</p>
+        </div>
+
+    </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+.cart {
+    height: 250px;
+    width: 360px;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%) translateY(110%);
+    border-radius: 10px;
+    background-color: $color-neutral-White;
+    box-shadow: 0px 20px 30px 0px rgb(0 0 0 / 20%);
+    cursor: initial;
+
+    &__title {
+        font-size: 1em;
+        color: $color-neutral-Black;
+        width: 100%;
+        height: 26%;
+        padding: 25px;
+        border-bottom: 1px solid $color-neutral-Grayish-blue;
+
+    }
+
+    &__content {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 74%;
+        font-weight: 700;
+    }
+}
+</style>
